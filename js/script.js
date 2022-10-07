558dabb4e74d1cf0d32f8a186c514d1c3a6724db
Hamzapab <hamzabouzeboudja31@gmail.com>
2022-10-07 19:29:41 +0200

    uploading the project ( half of work is done)

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// humberger nav menu toggle
+let hamburger = document.querySelector(".hamburger");
+let nav = document.querySelector("nav");
+
+function showandHideNav(){
+  hamburger.firstElementChild.classList.toggle("not-checked");
+  hamburger.firstElementChild.classList.toggle("checked");
+}
+hamburger.addEventListener("click",()=>{
+  showandHideNav();
+  if(hamburger.firstElementChild.classList.contains("checked")){
+    nav.style.width = "70%";
+    document.body.style.cssText = "overflow:hidden;"
+  }else{
+    nav.style.width ="0";
+    document.body.style.cssText = "overflow:auto;"
+  }
+})
+
+document.addEventListener("click", (e) => {
+  const isClosest = e.target.closest(".target");
+  if (!isClosest && hamburger.firstElementChild.classList.contains("checked")) {
+    showandHideNav();
+    nav.style.width ="0";
+    document.body.style.cssText = "overflow:auto;"
+    }
+  });
+
+// Statistics section
+
+let statistics = document.querySelector(".our-statistique");
+let boxDiv = document.querySelectorAll(".statis-box div span");
+let started = false; // Function Started ? No
+
+
+window.addEventListener("scroll",()=>{
+  if(window.scrollY >= statistics.offsetTop - 200){
+    if(!started){
+      boxDiv.forEach((box)=>{
+        startCount(box)
+      })
+    }
+    started = true;
+  }
+})
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      el.textContent = el.textContent + "+"
+      clearInterval(count);
+    }
+  }, 500 / goal);
+}
